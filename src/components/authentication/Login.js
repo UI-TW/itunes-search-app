64135eb4b86d45b7c7d67d4c4667d611d6d3fe86
Jinto Jose <jintoppy@gmail.com>
2018-02-04 09:29:25 +0530

    Adding material style to Login

--- src/components/authentication/Login.js
+++ src/components/authentication/Login.js
@@ -1,10 +1,28 @@
 import loginTemplate from './login.tpl.html';
+import './Login.css';
 
 class Login{
     constructor(){
 
     }
-
+    login(){
+        alert('login called');
+    }
+    signup(){
+        alert('signup called');
+    }
+    attachEventListener(){
+        document.querySelector('#search_result').addEventListener('click', (e) => {
+            if(e.target.id === 'btn-signup'){
+                e.preventDefault();
+                this.login();
+            }
+            else if (e.target.id === 'btn-login'){
+                e.preventDefault();
+                this.signup();
+            }
+        });
+    }
     render(){
         return loginTemplate();
     }
