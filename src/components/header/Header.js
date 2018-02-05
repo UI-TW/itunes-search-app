c9d30ab64d41eee832de245a2938d42352eb19b9
Jinto Jose <jintoppy@gmail.com>
2018-02-05 22:16:07 +0530

    logout functionality

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -9,35 +9,45 @@ class Header {
     this.render();
   }
 
+  onClick(e){
+    switch(e.target.id){
+      case 'appHeader': 
+        e.preventDefault();
+        state.activeView = 'search';
+        emitter.emit('viewChange', state.activeView);
+      break;
+      case 'loginLink':
+        e.preventDefault();
+        state.activeView = 'login';
+        emitter.emit('viewChange', state.activeView);
+      break;
+      case 'favLink':
+        e.preventDefault();
+        state.activeView = 'favorites';
+        emitter.emit('viewChange', state.activeView);
+      break;
+      case 'logoutLink':
+        e.preventDefault();
+        removeAuthToken();
+        state.isLoggedIn = false;
+        state.isGuest = true;
+        state.activeView = 'search';
+        emitter.emit('viewChange', state.activeView);
+        this.render();
+    }
+  }
+
   attachEventListeners(){
-    document.querySelector('#header').addEventListener('click', (e) => {
-      switch(e.target.id){
-        case 'appHeader': 
-          e.preventDefault();
-          state.activeView = 'search';
-          emitter.emit('viewChange', state.activeView);
-        break;
-        case 'loginLink':
-          e.preventDefault();
-          state.activeView = 'login';
-          emitter.emit('viewChange', state.activeView);
-        break;
-        case 'favLink':
-          e.preventDefault();
-          state.activeView = 'favorites';
-          emitter.emit('viewChange', state.activeView);
-        break;
-        case 'logoutLink':
-          e.preventDefault();
-          removeAuthToken();
-          state.activeView = 'search';
-          emitter.emit('viewChange', state.activeView);
-          this.render();
-      }
-    });
+    try{
+      document.querySelector('#header').removeEventListener('click', this.onClick.bind(this));
+    }catch(e){
+
+    }
+    document.querySelector('#header').addEventListener('click', this.onClick.bind(this));
   }
 
   render() {    
+    console.log(state);
     const html = headerTemplate(state);
     document.querySelector('#container #header').innerHTML = html;
     this.attachEventListeners();
