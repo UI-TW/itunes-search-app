8d66e9dbb972aa1cdf63dc13e5a4aee52324100c
pankajladhar <ladharpankaj@gmail.com>
2018-02-06 15:58:47 +0530

    Added Basic serviceworker | Pankaj

---
+++ PWA-sw.js
@@ -0,0 +1,21 @@
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function() {
+      navigator.serviceWorker.register('/PWA-sw.js').then(function(registration) {
+        // Registration was successful
+        console.log('%c ServiceWorker registration successful', 'color: #00ab07');
+      }, function(err) {
+        // registration failed :(
+          console.log('%c ServiceWorker registration failed', 'color: #ca0d2c');
+          console.log(err)
+      });
+    });
+  }
+
+  self.addEventListener('install', function(event) {
+    console.log('%c ServiceWorker install method', 'color: #FF5722');
+  });
+
+  self.addEventListener('activate', event => {
+    console.log('%c ServiceWorker activate method', 'color: #CDDC39');
+  });
+  
