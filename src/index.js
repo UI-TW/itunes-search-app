bd1a556c247b87e17651ce996545d16d21aa3453
Jagat Jeevan Sahoo <jagatsah@thoughtworks.com>
2018-02-06 08:37:59 +0530

    Performance improved from 34 to 64. Added manifest file, launcher icon, editorConfig for developer consistency, moved script files back to head with defer loading, changes to webpack config to compress css.

--- src/index.js
+++ src/index.js
@@ -2,3 +2,6 @@ import 'babel-polyfill';
 import App from './components/App';
 
 new App().render();
+
+// TODO change it to a better style
+document.querySelector('#initial-content').className += ' hide';
