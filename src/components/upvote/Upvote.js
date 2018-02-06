88173ab3ac9e2473e8cb21163d8683a2724348eb
Ramanathan <ramanathan@in-ramanathan.local>
2018-02-07 00:25:27 +0530

    sort upvote and styling

--- src/components/upvote/Upvote.js
+++ src/components/upvote/Upvote.js
@@ -8,12 +8,22 @@ class Upvote {
     this.getUpvotes();
   }
 
+  sortFavs(a, b) {
+    if (a.count > b.count) {
+      return -1;
+    }
+    if (a.count < b.count) {
+      return 1;
+    }
+    return 0;
+  }
+
   async getUpvotes() {
     try {
       state.status = 'loading';
       const resp = await fetch(apiSettings.upvote);
       const json = await resp.json();
-      state.upvotes = { ...json.favorites };
+      state.upvotes = {...json.favorites.sort(this.sortFavs)};
       this.render();
     } catch (e) {
       state.status = 'error';
