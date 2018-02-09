7bf9c0da867f3d6887d18cd60c18b401888a3d1a
Ramanathan <ramanathan@in-ramanathan.local>
2018-02-09 22:24:49 +0530

    moving all rendering logic to app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,18 @@
 import '../helpers';
 import Header from './header/Header';
-import { getApiUrl } from '../utils';
-import { getUserName } from '../utils/storageUtils';
+import {getApiUrl} from '../utils';
+import apiSettings from '../urlConfig';
+import {getUserName, getVapidKey} from '../utils/storageUtils';
 import Search from './search/Search';
-import Container from './container/Container';
+
+import Message from './Message/Message';
 import './App.css';
 import emitter from '../emitter';
 import state from '../state';
-import containerTemplate from './container/container.tpl.html';
+import appTemplate from './app.tpl.html';
+import Login from "./authentication/Login";
+import Upvote from "./upvote/Upvote";
+import List from "./list/List";
 
 class App {
   constructor() {
@@ -19,15 +24,15 @@ class App {
   async getSearchResult(headerState) {
     try {
       state.status = 'loading';
-      this.render();
+      new Message().render();
       const resp = await fetch(getApiUrl(headerState));
       const json = await resp.json();
-      state.data = { ...json };
-      state.status = json.resultCount ? '' : 'noContent';
+      state.data = {...json};
+      state.status = json.resultCount ? 'init' : 'noContent';
+      new List().render();
     } catch (e) {
       state.status = 'error';
     }
-    this.render();
   }
 
   setUserInfo() {
@@ -35,14 +40,64 @@ class App {
     state.email = getUserName();
   }
 
+  //START: {Adding Push} {3} out of {5}
+  subscribe(e) {
+    e.preventDefault();
+    const iconElement = e.currentTarget.querySelector('i');
+    if (iconElement.textContent === 'notifications_active'){
+      return;
+    }
+    navigator.serviceWorker.ready.then(function(reg) {
+      const vapidKey = getVapidKey();
+      console.log(vapidKey);
+      reg.pushManager.subscribe({
+        userVisibleOnly: true,
+        applicationServerKey: vapidKey
+      })
+        .then((subscription) => {
+          iconElement.textContent = 'notifications_active';
+          fetch(apiSettings.subscribe, {
+            method: 'POST',
+            body: JSON.stringify({
+              subscription: subscription
+            }),
+            headers: new Headers({
+              'Content-Type': 'application/json'
+            })
+          });
+        })
+        .catch((err) => {
+          if (Notification.permission === 'denied') {
+            console.warn('Permission for notifications was denied');
+          } else {
+            console.error('Failed to subscribe the user: ', err);
+          }
+        });
+    });
+  }
+
+  attachEventListeners() {
+    try {
+      document.querySelector('#subscribeLink').removeEventListener('click', this.subscribe.bind(this));
+    } catch (e) {
+      throw e;
+    }
+    document.querySelector('#subscribeLink').addEventListener('click', this.subscribe.bind(this));
+  }
+  //END: {Adding Push} {3} out of {5}
+
   render() {
     let appContainer = document.querySelector('#itunes-search');
-    appContainer.innerHTML = containerTemplate();
+    appContainer.innerHTML = appTemplate();
     new Header();
     if (state.activeView === 'search') {
       new Search();
+    } else if (state.activeView === 'login') {
+      new Login().render();
+    } else if (state.activeView === 'favorites') {
+      new Upvote().render();
     }
-    new Container();
+    this.attachEventListeners();
   }
 }
 
