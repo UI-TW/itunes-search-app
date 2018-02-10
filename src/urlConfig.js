04ab85fd55886db584cfb402e7ba67c2e810012e
Ramanathan <ramanathan@in-ramanathan.local>
2018-02-10 14:55:10 +0530

    change base route

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,5 +1,6 @@
 // const baseRoute = 'http://localhost:3030';
-const baseRoute = 'https://dry-temple-99897.herokuapp.com';
+// const baseRoute = 'https://dry-temple-99897.herokuapp.com';
+const baseRoute =  'https://itunes-search-iypahdbpmn.now.sh';
 const baseUrl = `${baseRoute}/api`;
 const apiSettings = {
   login: `${baseUrl}/accounts/login`,
