63ac9dba2267c3b1d07403e6b0640dcdccc5dbf5
Ramanathan <ramanathan@in-ramanathan.local>
2018-02-11 22:33:36 +0530

    add-exercise

--- workbox-cli-config.js
+++ workbox-cli-config.js
@@ -1,6 +1,8 @@
 module.exports = {
   "globDirectory": "dist/",
+//  <!-- Step 2a: Pre-cache minimal assets  -->
   "globPatterns": [],
+//  <!-- Step 2a: Pre-cache minimal assets  -->
   "swSrc": "src/sw.js",
   "swDest": "dist/sw.js",
   "globIgnores": [
