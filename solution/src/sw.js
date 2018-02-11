98c15035e2e5759f63da80c99638a81f4e52acb9
Ramanathan <ramanathan@in-ramanathan.local>
2018-02-11 22:14:58 +0530

    initial push

---
+++ solution/src/sw.js
@@ -0,0 +1,15 @@
+importScripts('workbox-sw.prod.v2.1.2.js');
+const workboxSW = new WorkboxSW();
+workboxSW.precache([]);
+
+// <!-- Step 1b: Add service worker lifecycle events -->
+self.addEventListener('install', function (event) {
+  console.log('%c ServiceWorker installation successful', 'color: #FF00ff');
+  event.waitUntil(self.skipWaiting());
+});
+
+self.addEventListener('activate', event => {
+  console.log('%c ServiceWorker activation successful', 'color: #CDDC39');
+  event.waitUntil(self.clients.claim());
+});
+// <!-- Step 1b: Add service worker lifecycle events -->
--- src/sw.js
+++ src/sw.js
@@ -1,3 +1,6 @@
 importScripts('workbox-sw.prod.v2.1.2.js');
 const workboxSW = new WorkboxSW();
 workboxSW.precache([]);
+
+// <!-- Step 1b: Add service worker lifecycle events -->
+// <!-- Step 1b: Add service worker lifecycle events -->
