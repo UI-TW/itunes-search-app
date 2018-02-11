d1991b07eeab2726abd128aa83c5cab0bc69181b
Ramanathan <ramanathan@in-ramanathan.local>
2018-02-11 23:01:36 +0530

    add current exercise

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,16 @@ import '../helpers';
 import './App.css';
 import appTemplate from './app.tpl.html';
 import Router from './Router';
+// <!-- Step 3a: Add subscribe icon to app -->
+// <!-- Step 3a: Add subscribe icon to app -->
 
 class App {
   render() {
     let appContainer = document.querySelector('#itunes-search');
     appContainer.innerHTML = appTemplate();
     new Router();
+    // <!-- Step 3a: Add subscribe icon to app -->
+    // <!-- Step 3a: Add subscribe icon to app -->
   }
 }
 
