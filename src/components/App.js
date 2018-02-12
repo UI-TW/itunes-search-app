59d56b58ea195aa2091754831c599fda60c90543
Ramanathan <ramanathan@in-ramanathan.local>
2018-02-12 22:17:09 +0530

    add subscribe icon by default

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,14 @@ import '../helpers';
 import './App.css';
 import appTemplate from './app.tpl.html';
 import Router from './Router';
+import Subscribe from './subscribe/Subscribe';
 
 class App {
   render() {
     let appContainer = document.querySelector('#itunes-search');
     appContainer.innerHTML = appTemplate();
     new Router();
+    new Subscribe();
   }
 }
 
